2155046f1633921be169947fabadd5d419ade0e8
AlejandroWaiz <alejosaurio1@gmail.com>
2025-03-13 23:16:00 -0300

    Added player as a whole entity instead of id for db tracking

--- backend/entitys/Match.go
+++ backend/entitys/Match.go
@@ -4,7 +4,9 @@ type Match struct {
 	ID                 int         `json:"id"`
 	RoundID            int         `json:"round_id"`
 	NumberOfEncounters int         `json:"number_of_encounters"`
-	WinnerID           int         `json:"winner_id"`
-	LoserID            int         `json:"loser_id"`
+	Player1            Player      `json:"player1"`
+	Player2            Player      `json:"player2"`
+	Player1Score       int         `json:"player1_score"`
+	Player2Score       int         `json:"player2_score"`
 	Encounters         []Encounter `json:"encounters,omitempty"`
 }
